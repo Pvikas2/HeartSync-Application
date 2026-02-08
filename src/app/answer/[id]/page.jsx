48e2909867d3bbe64ede5ff93c25d547c3205dcc
Vikas Pimpale <vikasp.mech1996@gmail.com>
2026-02-08 20:30:13 +0530

    Removed export

--- src/app/answer/[id]/page.jsx
+++ src/app/answer/[id]/page.jsx
@@ -1,10 +1,26 @@
-import AnswerClient from "./AnswerClient";
+// import AnswerClient from "./AnswerClient";
 
-// 1. Remove dynamicParams = true (this was the error source)
-// 2. We MUST return at least one ID for the build to succeed, 
-// even if it's a fake one.
+// // 1. Remove dynamicParams = true (this was the error source)
+// // 2. We MUST return at least one ID for the build to succeed, 
+// // even if it's a fake one.
+// export async function generateStaticParams() {
+//   return [{ id: 'template' }]; 
+// }
+
+// export default function Page() {
+//   return <AnswerClient />;
+// }
+
+import AnswerClient from "./AnswerClient";
+import { getAllQuestionSetIds } from "@/lib/db"; // You'd need to create this function
+
 export async function generateStaticParams() {
-  return [{ id: 'template' }]; 
+  // Fetch all existing question set IDs from your database
+  const ids = await getAllQuestionSetIds();
+  
+  return ids.map((id) => ({
+    id: id,
+  }));
 }
 
 export default function Page() {
